ff42e59833d13dea2087859d35a8ef4b0dd70f13
iMRUM <rodkin.imry@gmail.com>
2024-08-25 17:25:33 +0300

    non-relevant fields were removed

--- receiver.py
+++ receiver.py
@@ -1,4 +1,3 @@
-import threading
 from quic import QuicConnection
 
 LOOP_BACK_ADDR = '127.0.0.1'
@@ -6,16 +5,13 @@ LOCAL_PORT = 3492
 REMOTE_PORT = 33336
 LOCAL_ADDRESS = (LOOP_BACK_ADDR, LOCAL_PORT)
 REMOTE_ADDRESS = (LOOP_BACK_ADDR, REMOTE_PORT)
-STREAM_ID_1 = 1
-STREAM_ID_2 = 2
-OUTPUT_FILE_A = 'received_a.txt'
-OUTPUT_FILE_B = 'received_b.txt'
 
 
 def main():
     connection_id = 1  # Server
     quic_connection = QuicConnection(connection_id, LOCAL_ADDRESS, REMOTE_ADDRESS)
     quic_connection.receive_packets()
+
 
 if __name__ == '__main__':
     main()
--- sender.py
+++ sender.py
@@ -1,5 +1,3 @@
-import threading
-import os
 from quic import (QuicConnection)
 
 LOOP_BACK_ADDR = '127.0.0.1'
@@ -10,7 +8,7 @@ REMOTE_ADDRESS = (LOOP_BACK_ADDR, REMOTE_PORT)
 STREAM_ID_1 = 1
 STREAM_ID_2 = 2
 FILE_A = 'a.txt'
-FILE_B = 'b.txt'
+FILE_B = 'img.gif'
 
 
 def main():
@@ -19,8 +17,20 @@ def main():
     # Add two streams for the files
     stream1 = quic_connection.get_stream(initiated_by=connection_id, direction=0).stream_id
     stream2 = quic_connection.get_stream(initiated_by=connection_id, direction=1).stream_id
-    quic_connection.add_data_to_stream(stream1, QuicConnection.read_file(FILE_A))
-    quic_connection.add_data_to_stream(stream2, b'123456789123456789')
+    stream3 = quic_connection.get_stream(initiated_by=connection_id, direction=0).stream_id
+    """
+    stream4 = quic_connection.get_stream(initiated_by=connection_id, direction=0).stream_id
+    stream5 = quic_connection.get_stream(initiated_by=connection_id, direction=0).stream_id
+    stream6 = quic_connection.get_stream(initiated_by=connection_id, direction=0).stream_id
+    """
+    quic_connection.add_file_to_stream(stream1, FILE_B)
+    quic_connection.add_file_to_stream(stream1, FILE_A)
+    quic_connection.add_file_to_stream(stream1, FILE_A)
+    """
+    quic_connection.add_data_to_stream(stream4, QuicConnection.read_file(FILE_A))
+    quic_connection.add_data_to_stream(stream5, QuicConnection.read_file(FILE_A))
+    quic_connection.add_data_to_stream(stream6, b'123456789123456789')
+    """
     quic_connection.send_packets()
 
 
