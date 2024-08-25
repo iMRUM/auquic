9e0945e4ceae196402563e79abf46d992b5283a3
iMRUM <rodkin.imry@gmail.com>
2024-08-25 20:55:17 +0300

    constants are done

--- sender.py
+++ sender.py
@@ -1,5 +1,6 @@
 from quic import (QuicConnection)
 
+CONNECTION_ID = 0 # Client
 LOOP_BACK_ADDR = '127.0.0.1'
 LOCAL_PORT = 33336
 REMOTE_PORT = 3492
@@ -7,30 +8,20 @@ LOCAL_ADDRESS = (LOOP_BACK_ADDR, LOCAL_PORT)
 REMOTE_ADDRESS = (LOOP_BACK_ADDR, REMOTE_PORT)
 STREAM_ID_1 = 1
 STREAM_ID_2 = 2
-FILE_A = 'a.txt'
-FILE_B = 'img.gif'
+FILE_A = 'img.gif'
 
 
 def main():
-    connection_id = 0  # Client
-    quic_connection = QuicConnection(connection_id, LOCAL_ADDRESS, REMOTE_ADDRESS)
-    # Add two streams for the files
-    stream1 = quic_connection.get_stream(initiated_by=connection_id, direction=0).stream_id
-    stream2 = quic_connection.get_stream(initiated_by=connection_id, direction=1).stream_id
-    stream3 = quic_connection.get_stream(initiated_by=connection_id, direction=0).stream_id
-    """
-    stream4 = quic_connection.get_stream(initiated_by=connection_id, direction=0).stream_id
-    stream5 = quic_connection.get_stream(initiated_by=connection_id, direction=0).stream_id
-    stream6 = quic_connection.get_stream(initiated_by=connection_id, direction=0).stream_id
-    """
-    quic_connection.add_file_to_stream(stream1, FILE_B)
-    quic_connection.add_file_to_stream(stream2, FILE_B)
-    quic_connection.add_file_to_stream(stream3, FILE_B)
-    """
-    quic_connection.add_data_to_stream(stream4, QuicConnection.read_file(FILE_A))
-    quic_connection.add_data_to_stream(stream5, QuicConnection.read_file(FILE_A))
-    quic_connection.add_data_to_stream(stream6, b'123456789123456789')
-    """
+    quic_connection = QuicConnection(CONNECTION_ID, LOCAL_ADDRESS, REMOTE_ADDRESS)
+    # Add streams for the files
+    stream1 = quic_connection.get_stream(initiated_by=CONNECTION_ID, direction=0).stream_id
+    stream2 = quic_connection.get_stream(initiated_by=CONNECTION_ID, direction=1).stream_id
+    stream3 = quic_connection.get_stream(initiated_by=CONNECTION_ID, direction=0).stream_id
+    # Add files to the streams
+    quic_connection.add_file_to_stream(stream1, FILE_A)
+    quic_connection.add_file_to_stream(stream2, FILE_A)
+    quic_connection.add_file_to_stream(stream3, FILE_A)
+    # Start sending
     quic_connection.send_packets()
 
 
